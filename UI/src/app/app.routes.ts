15ed9c162c2d7ea64167406d4b5243a1a75668a9
Rohan Ankalkote <rohanankalkote@gmail.com>
2024-04-28 21:09:46 +0530

    Basic structure of website

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'maintenancce', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
